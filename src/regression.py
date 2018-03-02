252e4059573d6ee34625044992a37e60bc19f870
Jamie-UoG <2137284s@student.gla.ac.uk>
2018-03-02 03:46:52 +0000

    Fixed all my messy code
    plus lots of comments and removals

--- src/regression.py
+++ src/regression.py
@@ -3,11 +3,18 @@
 import numpy as np
 from scipy.interpolate import *
 from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.preprocessing import FunctionTransformer
+
 import math
 from sklearn.metrics import mean_squared_error
 from matplotlib.pyplot import *
 
 from sklearn.preprocessing import PolynomialFeatures
+
+minmax_scaler = preprocessing.MinMaxScaler()
+
+
 train_dir = "../training-data/"
 test_dir = "../test-data/"
 sub_dir = "../submissions/"
@@ -22,180 +29,218 @@ train_ans = np.loadtxt(train_file_y, delimiter=',', skiprows=1)[:,1]
 
 test_args = np.loadtxt(test_file, delimiter=',', skiprows=1)
 
-# Run a full train/test cycle
-# args:
-#   am_test - amount of data to be designated for testing
-def run(p_test, deg):
 
-    am_test = p_test
 
-    #print (str((train_ans.shape[0])) + "   numer of input elements.")
-    #print ("Splitting input into blocks of " + str(am_test))
+'''
+Runs a regression model and cross validation to give answer.
+reg - type of regression model to use
+scl - type of scaler to use
+deg - polynomial degree to use
+print - print out metrics and graphs
+per - percent to use per segment (0 for 'leave 1 out validation')
 
-    # Full matrices of input data
-    args = train_args.reshape(-1,6)
-    ans = train_ans.reshape(-1,1)
+'''
+def runAndTestWithCrossValidation(reg, scl, deg, print_b, per):
 
-    f_test_args = test_args.reshape(-1,6)
+    seg_n = int(per*(train_args.shape[0]))
+    iters = float(train_args.shape[0])/float(seg_n)
 
-    avg_models = []
-
-    if (p_test == 0):
-        i = 1
-        am_test = ans.shape[0]
-        axis = range(0,ans.shape[0])
-        plot(axis,ans.flatten(),'-')
-    else:
-
-        # Get number of iterations
-        # If we have n number of test elements, and l total elements
-        # We can do i = (l/n) iterations, taking the ith block of n elements in the input data each iteration
-        length = ans.shape[0]
-        i = math.floor(length/am_test)
-
-        # Log answes to graph
-        axis = range(0,ans.shape[0])
-        plot(axis,ans.flatten(),'-')
-
-    # Repeat the training and test for each iteration
-    # Print RMSE for each iteration test
-    # Plot test points
+    axis = range(0,train_args.shape[0])
     final_ans = np.array([])
 
-    for x in range(0, i):
+    for i in range(0, int(iters)):
 
-        model = None
+        # Segment our data
+        if (i == int(iters-1)):
+            tr_args = train_args[:(seg_n*i)]
+            tr_ans = train_ans[:(seg_n*i)]
 
-        # Matrix for the training independent variables
+            te_args = train_args[(seg_n*i):]
+            te_ans = train_ans[(seg_n*i):]
+        else:
 
-        f = train_args[(am_test*x):]
-        s = train_args[:-(am_test*(x+1))]
-        tr_args = np.concatenate((f,s))
+            f = train_args[:(seg_n*i)]
+            s = train_args[(seg_n*(i+1)):]
+            tr_args = np.concatenate((f,s))
 
-        # Matrix for the training dependent variables
-        f = train_ans[(am_test*x):]
-        s = train_ans[:-(am_test*(x+1))]
-        tr_ans = np.concatenate((f,s))
+            f = train_ans[:(seg_n*i)]
+            s = train_ans[(seg_n*(i+1)):]
+            tr_ans = np.concatenate((f,s))
 
-        # Train regression model
-        model = train(tr_args, tr_ans, deg)
+            te_args = train_args[seg_n*i:seg_n*(i+1)]
+            te_ans  = train_ans[seg_n*i:seg_n*(i+1)]
 
-        avg_models += [model.coef_]
+        # Do any scaler operations
+        if (scl == "minmax"):
+            tr_args = minmax_scaler.fit_transform(tr_args)
+            te_args = minmax_scaler.fit_transform(te_args)
+        elif (scl == "robust"):
+            tr_args = preprocessing.robust_scale(tr_args)
+            te_args = preprocessing.robust_scale(te_args)
+        elif (scl == "log"):
+            transformer = FunctionTransformer(np.log1p)
+            tr_args = transformer.transform(tr_args)
+            te_args = transformer.transform(te_args)
 
-        # Matrix for the testing independent variables
-        te_args = train_args[am_test*x:am_test*(x+1)].reshape(-1,6)
 
-        # Matrix for the testing dependent variables
-        te_ans = train_ans[am_test*x:am_test*(x+1)].reshape(-1,1)
+        # Convert to polynomial features
+        poly = PolynomialFeatures(degree=deg)
+        tr_args = poly.fit_transform(tr_args)
+        te_args = poly.fit_transform(te_args)
 
-        # Run model on test section of data
-        answer = test(model, te_args, deg)
+        # Get our linear regression model
+        if (reg == "normal"):
+            model = train(tr_args, tr_ans)
+        elif (reg == "lasso"):
+            model = trainLasso(tr_args, tr_ans)
+        elif (reg == "ridge"):
+            model = trainRidge(tr_args, tr_ans)
+        elif (reg == "elastic"):
+            model = trainElastic(tr_args, tr_ans)
+        else:
+            return
+
+        # Run test of validation segment
+        answer = test(model, te_args)
         final_ans = np.concatenate((final_ans, answer))
 
-        # Log to graph
-        ax = axis[am_test*x:am_test*(x+1)]
+        if (print_b):
+            print ("For segment " + str(i))
+            print ("    of size " + str(answer.shape[0]))
+            print ("    RMSE of " + str(math.sqrt(mean_squared_error(answer, te_ans))))
+
+    # Log to graph
+    if (print_b):
+
+        print ("Done all segments ")
+        print ("    Total size    " + str(final_ans.shape[0]))
+        print ("    Total RMSE of " + str(math.sqrt(mean_squared_error(final_ans, train_ans))))
+        plot(axis, final_ans.flatten(),'o')
+        plot(axis, train_ans.flatten(), 'ro')
+        show()
+
+
+'''
+Runs a regression model and creates submission file.
+reg - type of regression model to use
+scl - type of scaler to use
+deg - polynomial degree to use
+print - print out metrics and graphs
+
+'''
+def runAndSubmit(reg, scl, deg, print_b):
+
+    tr_args = train_args
+    tr_ans = train_ans
+
+    te_args = test_args
+
+    # Do any scaler operations
+    if (scl == "minmax"):
+        tr_args = minmax_scaler.fit_transform(tr_args)
+        te_args = minmax_scaler.fit_transform(te_args)
+    elif (scl == "robust"):
+        tr_args = preprocessing.robust_scale(tr_args)
+        te_args = preprocessing.robust_scale(te_args)
+    elif (scl == "log"):
+        transformer = FunctionTransformer(np.log1p)
+        tr_args = transformer.transform(tr_args)
+        te_args = transformer.transform(te_args)
+
+
+    # Convert to polynomial features
+    poly = PolynomialFeatures(degree=deg)
+    tr_args = poly.fit_transform(tr_args)
+    te_args = poly.fit_transform(te_args)
+
+    # Get our linear regression model
+    if (reg == "normal"):
+        model = train(tr_args, tr_ans)
+    elif (reg == "lasso"):
+        model = trainLasso(tr_args, tr_ans)
+    elif (reg == "ridge"):
+        model = trainRidge(tr_args, tr_ans)
+    elif (reg == "elastic"):
+        model = trainElastic(tr_args, tr_ans)
+    else:
+        return
+
+    answer = test(model, tr_args)
+    print (math.sqrt(mean_squared_error(answer, tr_ans)))
+
+    if (print_b):
+        ax = range(0, answer.shape[0])
         plot(ax, answer.flatten(),'o')
-
-    show()
-
-    poly = PolynomialFeatures(degree=deg)
-    te_ = poly.fit_transform(f_test_args)
-    results = model.predict(te_)
-    print (results)
-    saveResults(results)
-    plot(np.array(range(0,results.shape[0])), results.flatten(),'o')
-    show()
-    return
-    # Finally, show
-    # Print test performance
-    printPerformnce(final_ans, ans, "done")
-
-    # Create final model from average of all models
-    final_model = np.zeros(avg_models[0].shape)
-    for mod in avg_models:
-        print (mod)
-        final_model = final_model + mod
-    print (final_model)
-    print (len(avg_models))
-    final_model = (final_model)/(len(avg_models))
-    print (final_model)
-
-    print (dir(model))
-    print (model.get_params())
-
-    model = linear_model.LinearRegression()
-    model.intercept_=0
-    model.coef_= final_model
-    print (model.coef_)
-    poly = PolynomialFeatures(degree=deg)
-    te_ = poly.fit_transform(f_test_args)
-    results = model.predict(te_)
-    print (results)
-    saveResults(results)
-    plot(np.array(range(0,results.shape[0])), results.flatten(),'o')
-    show()
-
-
-
-def runAndTest(deg, print_bool):
-    args = train_args.reshape(-1,6)
-    ans = train_ans.reshape(-1,1)
-
-    f_test_args = test_args.reshape(-1,6)
-
-    model = train(args, ans, deg)
-
-    answer = test(model, args, deg)
-
-    print (math.sqrt(mean_squared_error(answer, ans)))
-
-    if (print_bool):
-        ax = range(0,ans.shape[0])
-        plot(ax, ans.flatten(),'o')
-        plot(ax, answer.flatten(),'-')
+        plot(ax, tr_ans.flatten(),'ro')
         show()
 
-    answer = test(model, f_test_args, deg)
+    answer = test(model, te_args)
 
-    if (print_bool):
-        ax = range(0,f_test_args.shape[0])
-        plot(ax, answer.flatten(),'-')
+    if (print_b):
+        ax = range(0, answer.shape[0])
+        plot(ax, answer.flatten(),'o')
         show()
-
-    saveResults(answer)
-
+    saveResults(answer.flatten())
 
 
-#-- Trains a model using the training data  --#
-def train(tr_args, te_ans, deg):
+'''
+Trains a model using training args and answers
 
-    # PolynomialFeatures (prepreprocessing)
-    poly = PolynomialFeatures(degree=deg)
-    tr_ = poly.fit_transform(tr_args)
+train - regular linear regression
+trainLasso - use lasso regression
+trainRidge - use ridge regression
+trainElastic - use elastic regression
+
+'''
+def train(tr_args, tr_ans):
 
     # Perform linear regression on training data and get model
-    ols = linear_model.LinearRegression(normalize=True)
-    model = ols.fit(tr_, te_ans)
+    lin = linear_model.LinearRegression()
+    model = lin.fit(tr_args, tr_ans)
 
-    # Print the model
-    #print (model.coef_)
+    return model
 
-    # Return model
+def trainLasso(tr_args, tr_ans):
+
+    # Perform lasso regression on training data and get model
+    lass = linear_model.Lasso(alpha=1)
+    model = lass.fit(tr_args, tr_ans)
+
+    return model
+
+def trainRidge(tr_args, tr_ans):
+
+    # Perform ridge regression on training data and get model
+    ridg = linear_model.Ridge(alpha=1)
+    model = ridg.fit(tr_args, tr_ans)
+
+    return model
+
+def trainElastic(tr_args, tr_ans):
+
+    # Perform elastic regression on training data and get model
+    elas = linear_model.ElasticNet(alpha=1, l1_ratio=0.5)
+    model = elas.fit(tr_args, tr_ans)
+
     return model
 
 
-#-- Runs a test and generates an answer for test data --#
-def test(model, te_args, deg):
+'''
+Tests a given model with arguments to get predictions
+
+'''
+def test(model, te_args):
 
     # Run model on test data
-    poly = PolynomialFeatures(degree=deg)
-    te_ = poly.fit_transform(te_args)
-    results = model.predict(te_)
+    results = model.predict(te_args)
 
     return results
 
 
-#-- Saves the output of a test to csv file --#
+'''
+Saves our result to the csv file
+
+'''
 def saveResults(results):
 
     # Header for output file
@@ -210,18 +255,6 @@ def saveResults(results):
     # Save to file
     np.savetxt(sub_dir+"my_submission.csv", output, fmt='%d', delimiter=",", header=header, comments="")
 
-
-def printPerformnce(answer, te_ans, iter):
-    rmse =  math.sqrt(mean_squared_error(answer, te_ans))
-
-    # print ("ITERATION:   " + str(iter))
-    # print ("\tOver " + str(answer.shape[0]) + " test entries:")
-    # print ("\tRMSE    " + str(rmse))
-
-    print(rmse)
-    return rmse
-
-
-
-for i in range(0,10):
-    runAndTest(i, True)
+runAndTestWithCrossValidation("elastic", "log", 3, True, 0.10)
+runAndTestWithCrossValidation("elastic", "log", 3, True, 0.10)
+runAndTestWithCrossValidation("elastic", "log", 2, True, 0.10)
